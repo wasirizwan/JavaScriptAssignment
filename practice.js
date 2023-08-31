1cc3826834495320baea890abac1364d9d9f19db
wasirizwan <wasimuhammad869@gmail.com>
2023-08-31 09:50:33 +0500

    first commit

---
+++ practice.js
@@ -0,0 +1,57 @@
+//  *!Question1
+//  alert("Hello World");
+
+// *?Question2
+// alert("Error! please enter a valid password");
+
+// *! Question3
+// alert("Welcome to JS Land...\n Happy Coding!");
+
+// *?Question4
+// alert("Wellcome to JS Land...");
+// alert("Happy Coding!\nprevent this message from creating additional dialogs");
+
+// *!Question5
+// alert("Hello...I can run JS through my web browser consol");
+
+                            // **CHAPTER #2
+
+// *?Question1
+// var username
+
+// *!Question2
+// var myNmae = ("Muhammad Wasi");
+
+// *?Question3
+// var message = ("Hello World");
+// alert(message);
+
+// *!Question4
+// var studentData = (alert("Muhammad Wasi"), alert("22 Years Old"), alert("Certified Mobile Application Developer"));
+// alert(username);
+
+// *?Question5
+// var food = ("PIZZA \nPIZZ \nPIZ \nPI \nP");
+// alert(food);
+
+// *?Question6
+// var email = ("wasimuhammad00@gmail.com");
+// alert("My Email Address" +" "+ email);
+
+// *!Question7
+// var Book = ("A Smarter Way To Learn Javascript");
+// alert("I am learning from the book" +" "+ Book);
+
+// *?Question8
+// var Text = ("Yeh! I can write HTML content through JAVASCRIPT");
+// document.write(Text);
+
+// *!Question9
+// var myString = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// alert(myString);
+ 
+
+
+
+
+
