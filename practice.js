1583ce2b1995ac183aff4603d6c13d44a851da52
wasirizwan <wasimuhammad869@gmail.com>
2023-09-07 03:36:22 +0500

    Task2

--- practice.js
+++ practice.js
@@ -1,3 +1,6 @@
+                        // TODO TASK#1
+                        // **CHAPTER #2
+                        //  TODO ALERTS
 //  *!Question1
 //  alert("Hello World");
 
@@ -15,6 +18,7 @@
 // alert("Hello...I can run JS through my web browser consol");
 
                             // **CHAPTER #2
+                            // TODO VARIABLES FOR STRINGS
 
 // *?Question1
 // var username
@@ -51,7 +55,115 @@
 // alert(myString);
  
 
+                            // TODO TASK #2
+                            // **CHAPTER #5
+                            // TODO MATH EXPRESSION
+
+// *?QUESTION1
+var num1 = 10;
+var num2 = 5;
+var sum = num1 + num2;
+
+document.write(sum + "<br>" + "<hr>");
+
+// *!QUESTION2
+var num1 = 10;
+var num2 = 5;
+var sub = num1 - num2;
+
+document.write(sub + "<br>");
 
 
+var num1 = 10;
+var num2 = 5;
+var mul = num1 * num2;
 
+document.write(mul + "<br>");
 
+var num1 = 10;
+var num2 = 5;
+var div = num1 / num2;
+
+document.write(div + "<br>" + "<hr>");
+
+// *?Question3
+var value = 5;
+document.write(value + "<br>");
+
+document.write(value + 1 + "<br>");
+
+document.write(value + 1 + 7 + "<br>");
+
+document.write(value + 1 + 7 - 1 + "<br>");
+
+document.write((value + 1 + 7 - 1) % 3 + "<br>" + "<hr>");
+
+// *!QUESTION4
+var priceForOneTicket = 600;
+
+// If we buy 5 tickets for a movie then the price is shown below
+
+document.write(priceForOneTicket * 5 + "<br>" + "<hr>");
+
+// *?QUESTION6
+var C = 25;
+// Now we convert Celsius into Fahrenhieht
+var Result = (25 * 9/5) + 32;
+
+document.write("25<sup>0</sup>C is" +" "+ Result + "<sup>0</sup>F" + "<br>");
+
+var F = 70;
+// Now we convert Fahrenhieht into Celsius
+var Result2 = (F - 32) * 5/9;
+
+document.write("70<sup>0</sup>F is" +" "+ Result2 + "<sup>0</sup>C" + "<hr>");
+
+// *!QUESTION7
+var priceOfItem1 = 650; 
+var QuantityOfItem1Is = 3; 
+var priceOfItem2 = 100; 
+var QuantityOfItem2Is = 7; 
+var shippingCharges = 100; 
+
+var Result3 = (priceOfItem1 * QuantityOfItem1Is + priceOfItem2 * QuantityOfItem2Is + shippingCharges);
+document.write("<h1>Shopping Cart</h1><br>");
+document.write("Price of item 1 is 650<br>Quantity of item 1 is 3<br>Price of item 2 100<br>Quantity of item 2 is 7<br>Shipping charges 100<br><br>Total cost of your order is" +" "+ Result3+ "<hr>");
+
+// *?QUESTION8
+var marksObtained = 804;
+var totalMarks = 980;
+document.write("<h1>MARKS SHEET</h1>")
+document.write("Total Marks 980 <br> Marks Obtained 804 <br>Percentage:" +" "+ marksObtained/totalMarks * 100 + "%" + "<br>" + "<hr>"); 
+
+// *!QUESTION9
+var USD1 = 300;
+var SaudiRiyal1 =  81;
+
+var Result4 = (10*USD1 + 25*SaudiRiyal1);
+document.write("<h1>Currency in PKR</h1>")
+document.write("Total currency in PKR:" +" "+ Result4 + "<br>" + "<hr>");
+
+// *?QUESTION10
+var number = 40;
+var Result5 = ((number+5)*10)/2;
+
+document.write(Result5 +"<br>" + "<hr>");
+
+// *!QUESTION11
+var currentYear = 2023;
+var birthYear = 2001;
+
+var age = currentYear - birthYear;
+document.write("<h1>Age Calculator</h1>")
+document.write("Current Year: 2023 <br> Birth Year: 2001 <br> Your age is: " + age + "<br>" + "<hr>");
+
+// *?QUESTION13
+var favouriteSnakes = "kurleez";
+var currentAge = 22;
+var estimatedMaximumAge = 60;
+var amountOfSnaksPerDay = 2;
+
+var Result6 = ((estimatedMaximumAge - currentAge) * amountOfSnaksPerDay)
+document.write("<h1>The Lifetime Supply Calculator</h1>")
+document.write("Favourite Snakes: kurleez <br> Current Age: 22 <br> Estimated Maximum Age: 60 <br> Amount Of Snaks Per Day: 2<br>You will need " + 
+Result6 +" Kurleez to last you until the ripe old age 60");
