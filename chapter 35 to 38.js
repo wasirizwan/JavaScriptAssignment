88bff7c55c41cebdfffab548d50ce06bf10bc0f8
wasirizwan <wasimuhammad869@gmail.com>
2023-10-13 14:32:59 +0500

    Task 35

---
+++ chapter 35 to 38.js
@@ -0,0 +1,54 @@
+// // todoQuestion #1
+
+// function date () {
+//     var a = new Date();
+//     document.write(a + "<br>");
+// }
+
+// date();
+
+// // todoQuestion #2
+// function user(firstName,lastName) {
+//     var fullname = firstName + " " + lastName
+//     document.write(fullname);
+// }
+// user("Muhammad", "Wasi");
+
+// // todoQuestion #3
+// function sum(num1, num2) {
+//  return num1 + num2;
+// }
+// document.write(sum(+prompt("Type a number"),+prompt("Type another number")));
+
+// todoQuestion #5
+// function square (num) {
+//     return num * num;
+// }
+// document.write(square(4));
+
+// todoQuestion #7
+
+// function countiing (input1, input2){
+//     for(i = input1; i <= input2; i++ ){
+//         document.write(i + "<br>");
+//     }
+// }
+// countiing(prompt("type a number"),prompt("type another number"));
+
+// todoQuestion #12
+
+// function fLongestWord(str) {
+//     var words = str.split(' ');
+//     var longestWord = '';
+
+//     for (let i = 0; i < words.length; i++) {
+//         if (words[i].length > longestWord.length) {
+//             longestWord = words[i];
+//         }
+//     }
+
+//     return longestWord;
+// }
+// document.write(fLongestWord("Web Development Tutorial"));    
+
+// todoQuestion #1
