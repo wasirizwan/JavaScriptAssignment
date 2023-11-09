a3ea2ce037eca4ccba47119a87b33097fb9185ce
wasirizwan <wasimuhammad869@gmail.com>
2023-11-09 05:35:33 +0500

    Task9

---
+++ chapter 48.js
@@ -0,0 +1,15 @@
+function alertCall(){
+    alert("Hello World");
+}
+
+function image1() {
+    alert("This is image one")
+}
+
+function image2() {
+    alert("This is image two")
+}
+
+function pic2(){
+    alert("https://tse2.mm.bing.net/th?id=OIP.z6ecQiiRwnO7ALzE79lVFAHaE8&pid=Api&P=0&h=220")
+}
